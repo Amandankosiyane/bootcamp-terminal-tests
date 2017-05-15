08202b8c963f1680d3366280b7cc733c9e551dbc
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-05-15 13:44:02 +0200

    fixed the error in the functon that i tested

--- regCheck.js
+++ regCheck.js
@@ -1,4 +1,4 @@
-module.export = function(regNum, location){
+module.exports = function(regNum, location){
 var registration =regNum.endsWith(location);
   console.log('registration', + " " + location);
   return registration;
