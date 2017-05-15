defe66b897dd870eb72de70a7f513f260d9d5c80
Amandankosiyane <amandankosiyane.etafeni@gmail.com>
2017-05-15 13:17:37 +0200

    made an error in a test

--- regCheck.js
+++ regCheck.js
@@ -1,4 +1,4 @@
-module.exports = function(regNum, location){
+module.export = function(regNum, location){
 var registration =regNum.endsWith(location);
   console.log('registration', + " " + location);
   return registration;
